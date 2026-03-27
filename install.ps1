f5d962ca389b07b00f92c9d3cc75c85f2b8618aa
notysozu <lifegamer2050@gmail.com>
2026-03-28 02:21:50 +0530

    feat(install.ps1): add dependency installation

--- install.ps1
+++ install.ps1
@@ -4,3 +4,5 @@ Write-Host "Detecting Windows Version: $([System.Environment]::OSVersion.Version
 $pkgMgr = "none"
 if (Get-Command choco -ErrorAction SilentlyContinue) { $pkgMgr = "choco" }
 elseif (Get-Command winget -ErrorAction SilentlyContinue) { $pkgMgr = "winget" }
+Write-Host "→ Installing system dependencies..."
+if ($pkgMgr -eq "choco") { choco install python nodejs -y }
